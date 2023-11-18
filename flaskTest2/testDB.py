3f2c553d1acef0526d41c424ed713226b80e8983
Strory <hstrory@mail.ru>
2023-11-18 10:49:59 +1000

    bd

--- flaskTest2/testDB.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect("my_data.sqlite")
-
-cur = con.cursor()
-# cur.execute("""INSERT INTO test (content) VALUES ('goodbye World')""")
-cur.execute("""INSERT INTO test (content) VALUES ('Lets GOOOO')""")
-con.commit()
